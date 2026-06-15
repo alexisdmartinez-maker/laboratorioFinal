a13fa1176123674a74abc1bb1a1c5a013a73424d
Andres Cruz <Redxwx18@outlook.com>
2026-06-15 15:49:46 -0600

    Mi mensaje de actualizacion

--- src/laboratorioFinal/Categoria.java
+++ src/laboratorioFinal/Categoria.java
@@ -1,5 +1,24 @@
 package laboratorioFinal;
 
 public class Categoria {
+	public class categoria {
+	    private int idCategoria;
+	    private String nombre;
 
+	    public void Categoria(int id, String nombre) {
+	        this.idCategoria = id;
+	        this.nombre = nombre;
+	    }
+
+	    public int getIdCategoria() { return idCategoria; }
+	    public void setIdCategoria(int id) { this.idCategoria = id; }
+	    
+	    public String getNombre() { return nombre; }
+	    public void setNombre(String nombre) { this.nombre = nombre; }
+
+	    @Override
+	    public String toString() {
+	        return "Categoria [idCategoria=" + idCategoria + ", nombre=" + nombre + "]";
+	    }
+	}
 }
